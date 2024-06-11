1033280406180098d90a569fd4f787f53c52bd9e
Desmond Karani <karanidesmond@proton.me>
2024-06-11 08:35:54 +0300

    homm

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
+"""
+Module 0-minoperations
+Contains function minOperations(n) that calculates the fewest
+number of operations
+needed to result in exactly n H characters in the file.
+"""
+
+
 def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result in
+    exactly n H characters.
+
+    Parameters:
+    n (int): The target number of characters.
+
+    Returns:
+    int: The minimum number of operations required, or 0 if n is
+    impossible to achieve.
+    """
     if n < 2:
         return 0
 
